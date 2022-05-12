e60b1478d716fb0d1af30524a552c73c1f172cf2
David <40641544+Sluxz@users.noreply.github.com>
2022-05-12 08:55:38 +0200

    test

--- generateImage.js
+++ generateImage.js
@@ -17,7 +17,7 @@ const av = {
 const generateImage = async (member) => {
     let username = member.user.username
     let discrim = member.user.discriminator
-    let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: "false", size: av.size})
+    let avatarURL = member.user.displayAvatarURL({format: "jpg", dynamic: "false", size: av.size})
 
     const canvas = Canvas.createCanvas(dim.width, dim.height)
     const ctx = canvas.getContext("2d")
@@ -57,7 +57,7 @@ const generateImage = async (member) => {
     ctx.font = "4opx Sans"
     ctx.fillText("to the server", dim.width/2, dim.height - dim.margin - 50)
 
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcum.jpg")
     return attachment
 }
 
